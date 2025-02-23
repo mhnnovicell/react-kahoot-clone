b64aefb1b13a65fcc078c63012ee29b69c45a024
mhn2012MikkelsPC\Mikkel Hornbech <mikkelhn1@hotmail.com>
2025-02-23 15:41:37 +0100

    Added new changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +10,37 @@ export default defineConfig({
     basicSsl({
       domains: ['https://localhost:5174'],
     }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      webp: { quality: 50 },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
   ],
   server: {
     https: true,
-    port: 5174
+    port: 5174,
   },
 });
