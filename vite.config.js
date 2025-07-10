37bfe5366d992597059619372f18001c2033176c
mhn2012MikkelsPC\Mikkel Hornbech <mikkelhn1@hotmail.com>
2025-07-10 18:24:59 +0200

    Added pwa functionality

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,69 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import viteImagemin from 'vite-plugin-imagemin';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      base: '/',
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        sourcemap: true,
+        globPatterns: [
+          '**/*.{js,css,html,ico,png,svg,jpg,jpeg,webp,woff,woff2,ts}',
+        ],
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Quizazoid',
+        short_name: 'Quizazoid',
+        description:
+          'Quizazoid is a quiz management system that allows you to create, manage, and analyze quizzes with ease.',
+        theme_color: '#501f87',
+        background_color: '#501f87',
+        icons: [
+          {
+            src: 'src/assets/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'src/assets//web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'src/assets//favicon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+        ],
+        screenshots: [
+          {
+            src: 'src/assets//web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            form_factor: 'narrow',
+            label: 'Quizazoid',
+          },
+          {
+            src: 'src/assets//web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            form_factor: 'wide',
+            label: 'Quizazoid',
+          },
+        ],
+      },
+    }),
     basicSsl({
       domains: ['https://localhost:5174'],
     }),
